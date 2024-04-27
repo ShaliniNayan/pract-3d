afa0fd85a7fe1b5431270539015c51efea24368b
Shalini Nayan <shalininayan@outlook.com>
2024-04-27 16:23:05 +0000

    Add basics about Next.js

---
+++ 01_basics/next.js
@@ -0,0 +1,42 @@
+//Next.js simpliflies the development process 
+// On top of that it optimizes your web app
+
+// Rendering :
+/*
+- Client - side rendering or browser rendering happens on client device or the browser.
+- Server - side rendering : Rendering the web page on the server before transmitting it to the client's device.
+
+This distinctive highlights an essential aspects of web development.
+
+SEO -> SEARCH ENGINE OPTIMIZATION : If search engine does not work properly, this issue is resolved by sending pre-rendered code directly to the client.
+This enables :
+--- Easy crawling
+--- Indexing
+this leading to improvked SEO
+
+SEO : is a crucial for optimizing a website's visiblity and ranking in search engine results.
+
+Banefits SEO : 
+-> Increased Organic Traffic
+-> Enchanced user experience
+-> Credibility & Trustworthiness
+-> Competitive advantage
+
+Routing : Next.js uses file-based routing system means the routing is handled by the file system.
+
+Fullstack:
+
+API ROUTES : Enabling the creation of severless functions to handle API requests.
+
+Severless API's in next.js are a way of creating API endpoints.
+Without the need for traditional server: 
+* It allows to build and deploy API's
+* Without managing server infrastructure 
+* Worrying about scaling their server as traffic increases.
+
+HMR (HOT MODULE RELOADING): From 1.3s to 131ms - 10 times less
+
+Automatic Code Splitting: 
+
+
+*/
