d884c7bf726d94033d93acdf82b9dfaf0bc37761
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-05-21 18:29:35 +0530

    Update Code

--- app/page.js
+++ app/page.js
@@ -9,8 +9,6 @@ import Image from 'next/image';
 // 3. Client Side Rendering (CSR)
 // 4. Incremental Static Regeneration (ISR)
 
-// Project Structure hold
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
