ed52f3343a76e3c758f3688179224fc998619107
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-05-27 00:38:22 +0530

    Add text to hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,11 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white `}>
+            Hi, I'm <span className='text-[#915eff]'>Shalini Nayan</span>
+          </h1>
+        </div>
       </div>
     </section>
   );
