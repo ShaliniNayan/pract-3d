df4e29f7fb8a35bfcaf9b1703a187d8ca354f58f
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-05-29 23:18:22 +0530

    Add and update computer files for 3d image

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pintLight intensity={1} />
+      <primitive onject={computer.scene} />
+    </mesh>
+  );
+};
+
+export default Computers;
