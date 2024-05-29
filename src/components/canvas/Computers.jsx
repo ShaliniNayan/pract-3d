b53dc6cdd9faaa24e9f732d9ffb8ec0d17b65014
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-05-29 23:36:04 +0530

    Add and pdate the model of three d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,9 +10,31 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
-      <pintLight intensity={1} />
+      <pointLight intensity={1} />
       <primitive onject={computer.scene} />
     </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <canvas
+      frameLoop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </canvas>
   );
 };
 
