15eb80060b08ab40bd1ede731d7c346276af0c59
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-04 12:57:37 +0530

    Update the position and scale

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,12 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
