8423813e6d641b8e7d166d100cdd17398d41fe05
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-11 23:31:13 +0530

    Fix the size of the image

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,16 +8,16 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    // <mesh>
+    //   <hemisphereLight intensity={0.15} groundColor='black' />
+    //   <pointLight intensity={1} />
+    //   <primitive
+    //     object={computer.scene}
+    //     scale={0.75}
+    //     position={[0, -3.25, -1.5]}
+    //     rotation={[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
   );
 };
 
