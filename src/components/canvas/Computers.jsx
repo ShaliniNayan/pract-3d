0edb8cf0a4e8cddf435cc4a9d85c481696d67983
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-12 21:45:05 +0530

    Update scale and position

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,7 +15,6 @@ const Computers = () => {
         object={computer.scene}
         scale={0.75}
         position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
