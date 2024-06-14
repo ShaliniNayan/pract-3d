e8bce5ad26214560790a4f9014f1a9d012343bc5
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-14 18:05:25 +0530

    Fix with size of the image

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,12 +9,13 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={2.5} groundColor='black' />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={0.45}
+        position={[0, -4.5, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
