b8cd7037318c80a6453f99cc7b8158e9829f75a0
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-15 18:25:12 +0530

    Add and update motion paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,20 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled full-stack developer with passionate knowledge of
+        JavaScript, React, and Ruby on Rails and expertise in writing clean,
+        efficient, and maintainable code. I'm also a fast learner and a team
+        player who thrives in a collaborative environment with a strong work
+        ethic. I'm a self-starter who is always looking to improve my skills.
+        I'm a quick learner and collabrate closely with clients to create
+        efficient, scalable, and user-friendly solutions that slove real-world
+        problems. Let's work together to bring your ideas to life!
+      </motion.p>
     </>
   );
 };
