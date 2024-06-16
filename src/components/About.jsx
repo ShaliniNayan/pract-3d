2070d9e36d58feb9299c856d9dfbf12fa7fa0f09
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-16 20:26:06 +0530

    Add box image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,17 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
-        <div></div>
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex
+          justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+        </div>
       </motion.div>
     </Tilt>
   );
