5f4651c57e284505a2bbaaeb574cf454c6e52b73
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-18 23:35:58 +0530

    Update experience fine and add vertical time line

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,6 +18,14 @@ const Experience = () => {
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   );
 };
