2e3208e7413cb61012ba601bf35e31f5fccaf57f
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-18 23:05:01 +0530

    Add experience in components folder and update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Experience
+export default Experience;
