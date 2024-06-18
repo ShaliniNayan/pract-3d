238f8acbf721f5e4e1bfe369ad6f668c3e2b3f11
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-18 23:54:05 +0530

    Add image in vertical timeline elements in experience file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,7 +19,11 @@ const ExperienceCard = ({ experience }) => {
     iconStyle={{ background: experience.iconBg }}
     icon={
       <div>
-        <img />
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className='w-[60%] h-[60%] object-contain'
+        />
       </div>
     }
   >
