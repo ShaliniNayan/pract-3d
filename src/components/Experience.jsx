c099100785b758cc072274e002204c1a4b6c4784
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-19 22:42:59 +0530

    Add paragrah for experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -36,6 +36,17 @@ const ExperienceCard = ({ experience }) => (
         {experience.company_name}
       </p>
     </div>
+
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className='text-white-100 text-[14px] pl-1 tracking-wider'
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 );
 
