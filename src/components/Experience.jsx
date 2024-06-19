d57fbbf7490b033d3a20ed20c298294a7e8f719d
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-19 22:34:18 +0530

    Add paragrah and styling for paragraph

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -29,6 +29,12 @@ const ExperienceCard = ({ experience }) => (
   >
     <div className='flex justify-center items-center w-full h-full'>
       <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      <p
+        className='text-secondary text-[16px] font-semibold'
+        style={{ margin: 0 }}
+      >
+        {experience.company_name}
+      </p>
     </div>
   </VerticalTimelineElement>
 );
