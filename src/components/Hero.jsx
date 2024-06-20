3f7442e032ed233948f87dae0cc2ea084a163321
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-20 13:16:39 +0530

    Fix the error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,10 +19,10 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Shalini Nayan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Passionate Full-Stack Software Developer{' '}
+            Passionate Full-Stack Software Developer
             <br className='sm:block hidden' />
-            in React, Ruby on Rails, and writing reusable code for efficient,
-            user-friendly systems.
+            in React, Ruby on Rails, and writing reusable code for
+            efficient,user-friendly systems.
           </p>
         </div>
       </div>
