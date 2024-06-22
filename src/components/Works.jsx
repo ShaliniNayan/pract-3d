a6d73bed55e529295e1a6688475198d4f9054aec
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-22 22:45:28 +0530

    Add project card in work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,64 +8,64 @@ import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
-// const ProjectCard = ({
-//   index,
-//   name,
-//   description,
-//   tags,
-//   image,
-//   source_code_link,
-// }) => {
-//   return (
-//     <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
-//       <Tilt
-//         options={{
-//           max: 45,
-//           scale: 1,
-//           speed: 450,
-//         }}
-//         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-//       >
-//         <div className='relative w-full h-[230px]'>
-//           <img
-//             src={image}
-//             alt='project_image'
-//             className='w-full h-full object-cover rounded-2xl'
-//           />
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
 
-//           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-//             <div
-//               onClick={() => window.open(source_code_link, '_blank')}
-//               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-//             >
-//               <img
-//                 src={github}
-//                 alt='source code'
-//                 className='w-1/2 h-1/2 object-contain'
-//               />
-//             </div>
-//           </div>
-//         </div>
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        </div>
 
-//         <div className='mt-5'>
-//           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-//           <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-//         </div>
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
 
-//         <div className='mt-4 flex flex-wrap gap-2'>
-//           {tags.map((tag) => (
-//             <p
-//               key={`${name}-${tag.name}`}
-//               className={`text-[14px] ${tag.color}`}
-//             >
-//               #{tag.name}
-//             </p>
-//           ))}
-//         </div>
-//       </Tilt>
-//     </motion.div>
-//   );
-// };
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
 
 const Works = () => {
   return (
