9e593e503b0ee38e52200a24d4e722d01c2e063b
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-24 18:05:39 +0530

    Fix the eroor for image

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,8 @@ import redux from './tech/redux.png';
 import tailwind from './tech/tailwind.png';
 import typescript from './tech/typescript.png';
 import threejs from './tech/threejs.svg';
+import ruby from './tech/ruby.png';
+import rails from './tech/rails.png';
 
 import meta from './company/meta.png';
 import shopify from './company/shopify.png';
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  rails,
+  ruby,
 };
