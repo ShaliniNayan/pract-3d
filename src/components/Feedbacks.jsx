7f9a00bd884663326c05d4f344c3957bfdb57544
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-30 23:22:19 +0530

    Add and update feedback components

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Feedbacks = () => {
-  return (
-    <div>Feedbacks</div>
-  )
-}
+  return <div>Feedbacks</div>;
+};
 
-export default Feedbacks
+export default Feedbacks;
