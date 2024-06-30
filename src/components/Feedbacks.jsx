409bf85e84346c6fa97c0cbee38203d2aa92d9e7
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-06-30 23:33:55 +0530

    Update styles for card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,9 +3,18 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
 
 const Feedbacks = () => {
-  return <div>Feedbacks</div>;
+  return (
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div></motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Feedbacks;
