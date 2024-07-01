aaa5ed5d56d16f2867c1fd028eec7c7b8b13ad62
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-01 21:52:26 +0530

    Update styling for testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -13,7 +13,10 @@ const FeedbackCard = ({
   company,
   image,
 }) => (
-  <motion.div variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
+  <motion.div
+    variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
     <p>{testimonial}</p>
   </motion.div>
 );
