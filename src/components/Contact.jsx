075f0619d068a39b859623573393d75fc0d11ba8
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 14:13:41 +0530

    Add all the imports to contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+
+function Contact() {
+  return <div>Contact</div>;
+}
+
+export default Contact;
