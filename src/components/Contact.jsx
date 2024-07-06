2c12a531e258f2d987730413b59de98c5e823a11
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 14:49:06 +0530

    Add form tag and update styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,6 +28,12 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        ></form>
       </motion.div>
     </div>
   );
