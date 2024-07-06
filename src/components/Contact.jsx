4dec9d0213d6f0530e0ed584d509322abbe04d86
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 14:22:35 +0530

    Add form to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,9 +5,17 @@ import emailjs from '@emailjs/browser';
 import { styles } from '../styles';
 import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
-function Contact() {
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
   return <div>Contact</div>;
-}
+};
 
 export default Contact;
