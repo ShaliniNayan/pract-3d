60781ea3a1f0e0f2dd6433aa365e0289ed52264a
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 14:39:47 +0530

    Add styling for paragrah and heading in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,8 +26,8 @@ const Contact = () => {
         variants={slideIn('left', 'tween', 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
-        <p>Get in touch</p>
-        <h3>Contact</h3>
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
       </motion.div>
     </div>
   );
