788489918bceeff266b7769a65ef08e1c4c3adff
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 15:35:03 +0530

    Add  styling in label name and submit button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
@@ -70,7 +70,7 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
           >
             {loading ? 'Sending...' : 'Submit'}
           </button>
