4fb29ab4ebd94adcc06d00dfbea3d7b9d9eaaab2
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-06 15:21:10 +0530

    Update all the labels in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,24 +46,24 @@ const Contact = () => {
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-white font-medium mb-4'>Your Email</span>
             <input
-              type='text'
-              name='name'
-              value={form.name}
+              type='email'
+              name='email'
+              value={form.email}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your email?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows='7'
+              name='message'
+              value={form.message}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder='What do you want to say?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
             />
           </label>
