5b92cf3c5c31093e040b264f0a591bcb15bb2093
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-07 15:47:59 +0530

    Add earth and update the earth canvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '..//Loader';
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  return <div>Earth</div>;
+};
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
