4d1a164b7df1a56fed8075156b5f43db844103ad
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-07 15:57:47 +0530

    Error fixed

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,8 +6,9 @@ import CanvasLoader from '..//Loader';
 
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf');
+
   return (
-    <primitive object={earth.scene} scale={2.5} position={0} rotation={0} />
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
