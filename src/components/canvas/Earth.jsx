c22a047a668ba6f91e071881154596caae879d81
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-09 19:59:08 +0530

    Update camera view 3d image

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -18,7 +18,12 @@ const EarthCanvas = () => {
       shadows
       frameloop='demand'
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
