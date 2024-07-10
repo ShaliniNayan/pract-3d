2d3863bcc424a5901cc63f5ee548707d6fc54467
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-10 09:49:54 +0530

    Add stars components canvas and import files

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
+  return <div>Stars</div>;
+};
 
-export default Stars
+export default Stars;
