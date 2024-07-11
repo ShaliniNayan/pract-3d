76c6140725792b4dc13a09f76deddd3b1b6407ab
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-11 20:55:44 +0530

    Update star element

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,8 +3,18 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
-  return <div>Stars</div>;
-};
+const Stars = (props) => {
+  return (
+    <div>Stars</div>
+  )
+}
 
-export default Stars;
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1]'>
+      <Canvas ></Canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas;
