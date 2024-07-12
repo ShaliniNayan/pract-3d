f21851f73ec5e61fe8e14e169187076552ddf99b
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-12 17:11:33 +0530

    Update camera position for star in canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,17 +4,21 @@ import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
-  return (
-    <div>Stars</div>
-  )
-}
+  return <div>Stars</div>;
+};
 
 const StarsCanvas = () => {
   return (
     <div className='w-full h-full absolute inset-0 z-[-1]'>
-      <Canvas ></Canvas>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
     </div>
-  )
-}
+  );
+};
 
 export default StarsCanvas;
