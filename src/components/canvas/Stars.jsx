f9d19172fcc4dcd7dca1ad2d7550d08c6bee34c6
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-13 21:39:08 +0530

    Update rotation x and y for star rotation

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,6 +8,11 @@ const Stars = (props) => {
 
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
