e724c68af29b7c0eb4dbca994881b71f37fc630f
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-13 20:41:14 +0530

    Add properties for stars canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -10,7 +10,15 @@ const Stars = (props) => {
 
   return (
     <group>
-      <Points ref={ref} positions={sphere} />
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
     </group>
   );
 };
