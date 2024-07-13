800f03bcee4661c438dbe59e9ad75d949a3afe0f
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-07-13 20:53:55 +0530

    Update styling star canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -25,7 +25,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-full absolute inset-0 z-[-1]'>
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
